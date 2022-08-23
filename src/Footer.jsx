70ee402d94663426dcc4ecf96e812552be7e392a
Furkan Karaca <devfkn@gmail.com>
2022-08-23 22:32:26 +0300

    yeni_baslangic

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  
+  return (
+    <footer>
+        
+        <p>Copyrighted by Krc {new Date().getFullYear()}</p>
+
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Note() {
+  return (
+    <div className='note'>
+        
+        <h1 className="title">React bitirilecek</h1>
+        <p className="content">Toplam proje sayımız 100 olana kadar devam.</p>
+
+    </div>
+  )
+}
+
+export default Note
