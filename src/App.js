a340d81b0bfd4a2ec16c46abd1799e53897cc746
Furkan Karaca <devfkn@gmail.com>
2022-08-25 22:05:53 +0300

    KeeperApp_tamam

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import './App.css';
 import Header from './Header';
 import Note from './Note'
 import Footer from './Footer';
+import CreateArea from './CreateArea';
+import { useState } from 'react';
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote]
+    })
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div className="App">
       <Header />
-      <Note />
-      
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
